c9310fe24bbf5cc8f84da0365064157612c9f562
xiyang <1042335226@qq.com>
2024-12-29 21:49:17 +0800

    add c++ template cmake

---
+++ TemplateRepoCxx/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++20", "World", "from", "VS Code", "and the C++ extension!","https://code.visualstudio.com/docs/cpp/config-linux"};
+    
+    for (const string& word : msg)
+    {
+        cout << word << " "<<endl;
+    }
+    cout << endl;
+}
